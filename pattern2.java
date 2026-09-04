8fcce6decdec77a5a6ddfaa6283127500676e4c3
Rutuja Kale <rutujakale067@gmail.com>
2026-09-04 22:18:50 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,13 @@
+//inverted pattern 
+public class pattern2 {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i =1; i<=n;i++){
+            for (int star = 1; star <=n-i+1; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}

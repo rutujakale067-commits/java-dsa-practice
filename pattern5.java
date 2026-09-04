ac5c11018067dcf409045f9ebaddc3161ea1d87e
Rutuja Kale <rutujakale067@gmail.com>
2026-09-04 23:55:50 +0530

    Add files via upload

---
+++ pattern5.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class pattern5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+} 
+    

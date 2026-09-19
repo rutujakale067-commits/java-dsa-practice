231b2b6a37d4caf0026484c330860ec03b759a2d
Rutuja Kale <rutujakale067@gmail.com>
2026-09-20 00:16:56 +0530

    Add files via upload

---
+++ funtion.java
@@ -0,0 +1,12 @@
+public class funtion{
+    public static int add(int a,int b){  // method 
+        int sum  =  a+b; // addition of 2 numbers
+        return sum;  // return statement
+         
+    }
+    public static void main(String[] args) {
+        int sum = add(2, 3);
+
+        System.out.println(sum);
+    }
+}
